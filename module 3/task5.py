9cc0653bdc08b24f38aeee3bcea555868d00f28c
LuckyPrograme <157396472+LuckyPrograme@users.noreply.github.com>
2024-12-30 11:22:47 +0300

    Create task5.py

---
+++ module 3/task5.py
@@ -0,0 +1,15 @@
+count = 0
+for i in range(10**6, 2*(10**6)):
+    first = 1
+    second = 1
+    n = i
+    while n > 10:
+        last = n % 10
+        if len(str(n)) >= 4:
+            second *= last
+        else:
+            first *= last
+        n //= 10
+    if first == second:
+        count += 1
+print(count)
